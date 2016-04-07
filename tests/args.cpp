bf35570414882082884be716230d717f692b0da5
o <o@o1o.ch>
2016-04-07 23:39:28 +0200

    moar tests

--- tests/args.cpp
+++ tests/args.cpp
@@ -53,3 +53,54 @@ TEST(Args, named) {
   checkInt(f0(), 3);
 }
 
+TEST(Args, named_pos) {
+  Builder f0, f1;
+
+  f1 << BC::enter_fun     << 2
+     << BC::store         << b
+     << BC::store         << a
+     << BC::load          << a  << BC::force
+     << BC::load          << a  << BC::force
+     << BC::add
+     << BC::leave_fun
+     << BC::ret;
+
+  f0 << BC::enter_fun            << (int)0
+     << BC::mkclosure            << f1()    << L({a, b})
+     << BC::push                 << C(2)
+     << BC::push                 << C(1)
+     << BC::call_name_arg_adapt  << L({b, _})
+
+     << BC::push                 << C(1)
+     << BC::add
+     << BC::leave_fun
+     << BC::ret;
+
+  checkInt(f0(), 3);
+}
+
+TEST(Args, named_missing) {
+  Builder f0, f1;
+
+  f1 << BC::enter_fun     << 2
+     << BC::store         << b
+     << BC::store         << a
+     << BC::load          << a  << BC::force
+     << BC::load          << a  << BC::force
+     << BC::add
+     << BC::leave_fun
+     << BC::ret;
+
+  f0 << BC::enter_fun            << (int)0
+     << BC::mkclosure            << f1()    << L({a, b})
+     << BC::push                 << C(1)
+     << BC::call_name_arg_adapt  << L({a})
+
+     << BC::push                 << C(1)
+     << BC::add
+     << BC::leave_fun
+     << BC::ret;
+
+  checkInt(f0(), 3);
+}
+
